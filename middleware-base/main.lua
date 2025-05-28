b77c26eb71db8b913262ca3415d313400cc99466
Ryzh <gooddmitriy@mail.ru>
2025-05-28 23:49:11 +0300

    middleware

---
+++ middleware-base/main.lua
@@ -0,0 +1,85 @@
+local inspect = require("inspect").inspect
+
+local server = Astra.http.server:new()
+
+--#region Middleware
+
+local function my_middleware(next_handler)
+    ---@param request Request
+    ---@param response Response
+    return function(request, response, ctx)
+        -- Pre-handler logic
+        if "something wrong" then
+            return "Waaait a minute."
+        end
+        local result = next_handler(request, response, ctx)
+        -- Post-handler logic
+        if "you came up with a use case" then
+            local things = "Do some on-Leave logic"
+        end
+        return result
+    end
+end
+
+local function context(next_handler)
+    ---@param request Request
+    ---@param response Response
+    return function(request, response)
+        local ctx = {}
+        ctx.data_for_next_handler = 42
+        local result = next_handler(request, response, ctx)
+        return result
+    end
+end
+
+local function logger(next_handler)
+    ---@param request Request
+    ---@param response Response
+    ---@param ctx table
+    return function(request, response, ctx)
+        -- Entry part
+        print("Request:", request:method(), request:uri(), inspect(request:queries()))
+        local result = next_handler(request, response, ctx)
+        -- Leave part
+        return result
+    end
+end
+
+
+
+---Chains route middleware
+local function chain(chain)
+    return function(handler)
+        assert(type(handler) == "function",
+            "Handler must be a function, got " .. type(handler))
+        assert(#chain >= 2, "Chain must have at least 2 middlewares")
+        for i = #chain, 1, -1 do
+            local middleware = chain[i]
+            assert(type(middleware) == "function",
+                "Middleware must be a function, got " .. type(middleware))
+            handler = middleware(handler)
+        end
+        return handler
+    end
+end
+--#endregion
+
+---@param req Request
+---@param res Response
+local function homepage(req, res)
+    res:set_header("Content-Type", "text/html")
+    return "Hi there!"
+end
+
+---@param req Request
+---@param res Response
+---@param ctx table
+local function someHandler(req, res, ctx)
+    return "I got `" .. tostring(ctx.data_for_next_handler) .. "` from context"
+end
+
+
+server:get("/", logger(homepage))
+server:get("/ctx", chain { context, logger } (someHandler)) -- same as context(logger(someHandler))
+
+server:run()
---
+++ middleware-base/readme.md
@@ -0,0 +1,31 @@
+This example shows how to implement middleware.
+
+The insperation was taken from [Clojure](https://clojure.org/) community. To dig deeper, you can read about *interseptors* in [Pedestal documentation](https://pedestal.io/pedestal/0.7/guides/what-is-an-interceptor.html#_queue_and_stack).
+
+
+In short, each route has a root handler that is wrapped in another function, which is wrapped in another function, which is wrapped... you got it. And also we pass a context table as a third argument for inter-middleware communication.
+
+Here is a *best effort* visualization:
+
+![Astra Middleware visualisation](Astra_Middleware_Visualization.png)
+
+
+Each middleware takes a `next_handler` function which can be a root handler an another middleware.
+Each middleware has `Entry` and `Leave` part. If everything is ok in Entry logic, middleware calls `next_handler`, otherwise, it can return early.
+
+```lua
+local function my_middleware(next_handler)
+    return function(request, response, ctx)
+        -- Pre-handler logic
+        if "something wrong" then
+            return "Waaait a minute."
+        end
+        local result = next_handler(request, response, ctx)
+        -- Post-handler logic
+        if "you came up with a use case" then
+            local things = "Do some on-Leave logic"
+        end
+        return result
+    end
+end
+```
