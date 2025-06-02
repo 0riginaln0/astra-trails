8e7b4f6c0daa276b878ef9b0e46021451a6510a0
Ryzh <gooddmitriy@mail.ru>
2025-06-02 12:22:10 +0300

    snake_case yooo

--- quickstart/main.lua
+++ quickstart/main.lua
@@ -1,9 +1,11 @@
 require "globals"
 local middleware = require "middleware"
-local chain, context, logger, html = middleware.chain, middleware.context, middleware.logger, middleware.html
+local html = middleware.html
+local context = middleware.context
+local logger = middleware.logger
+local chain = middleware.chain
 
 local server = Astra.http.server:new()
-
 
 local templates = Astra.new_templating_engine("templates/**/*.html")
 templates:context_add("favourite_languages", "Lua, Elixir, C, Rust")
@@ -13,31 +15,21 @@ local function favlangs()
     return rendered_static_template
 end
 
-local function tierList(req)
+local function tier_list(req)
     local q = req:queries()
-    templates:context_add("name", q.name)
-    templates:context_add("s", q.s)
-    templates:context_add("a", q.a)
-    templates:context_add("b", q.b)
-    templates:context_add("c", q.c)
-    templates:context_add("d", q.d)
-    templates:context_add("e", q.e)
-    templates:context_add("f", q.f)
-
+    for _, param in ipairs {"name", "s", "a", "b", "c", "d", "e", "f"} do
+        templates:context_add(param, q[param])
+    end
     return templates:render("dynamic/tier-list.html")
 end
 
-local homepage_info = {
-    "I'm a homepage"
-}
+local homepage_info = { "I'm a homepage" }
 
 local function homepage()
     return table.concat(homepage_info, "\n")
 end
 
----@param req Request
----@param res Response
-local function addHomepageInfo(req, res)
+local function add_homepage_info(req, res)
     local new_info = req:queries().info
     if new_info then
         table.insert(homepage_info, new_info)
@@ -46,11 +38,8 @@ local function addHomepageInfo(req, res)
     return "Failed to add new info"
 end
 
----@param request Request
----@param response Response
-local function justHi(request, response)
-    local queries = request:queries()
-    local name = queries.name
+local function just_hi(request, response)
+    local name = request:queries().name
     if name then
         return "Hello " .. name .. "!"
     end
@@ -60,16 +49,16 @@ end
 Routes(server) {
     base_middleware = chain { context, logger },
     { GET,         "/",       homepage },
-    { POST,        "/",       addHomepageInfo },
-    { GET,         "/hi",     justHi },
+    { POST,        "/",       add_homepage_info },
+    { GET,         "/hi",     just_hi },
     { STATIC_FILE, "/main",   "main.lua" },
     { STATIC_DIR,  "/public", "public" },
 }
 
 Routes(server) {
-    base_middleware = chain { logger, html },
-    { GET, "/tier-list", tierList },
-    { GET, "/favlangs",  favlangs },
+    { GET, "/favlangs",  html (favlangs) },
+    { GET, "/tier-list", chain { logger, html } (tier_list) },
 }
 
 server:run()
+
