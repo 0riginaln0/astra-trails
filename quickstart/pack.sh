48b6bb658797f17fe6c793562e9bb675d6715175
Ryzh <gooddmitriy@mail.ru>
2025-12-07 11:20:32 +0300

    script for packing quickstart

---
+++ quickstart/pack.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create the quickstart directory
+mkdir -p quickstart
+
+# Copy files and folder into quickstart
+cp main.lua readme.md watch.sh quickstart/
+cp -r trails quickstart/
+
+# Create a zip archive of the quickstart folder
+zip -r quickstart.zip quickstart/
+
+rm -rf quickstart
+
+echo "Files packed into quickstart.zip"
