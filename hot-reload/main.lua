f365d9b0b4f5ec1846de0e986a69fe21023ef17a
0riginaln0 <74508026+0riginaln0@users.noreply.github.com>
2025-12-08 08:17:36 +0300

    Update main.lua

--- hot-reload/main.lua
+++ hot-reload/main.lua
@@ -6,13 +6,10 @@ local h2 = require("lib.some")
 server:get("/", function (request, response)
     return "Hello there!"
 end)
-
 server:get("/1", handlers.one)
-
 server:get("/2", handlers.two)
-
 server:get("/3", h2.three)
-
 server:get("/4", h2.four)
+server:static_file("/text", "assets/hey.txt")
 
 server:run()
